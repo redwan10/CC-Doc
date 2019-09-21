6736351203875b1e627aebadc94a283b01783897
Redwan Hussain <hussain.redwan10@gmail.com>
2019-09-22 03:01:08 +0600

    merge done

--- Menu-Three--01/assets/js/main.js
+++ Menu-Three--01/assets/js/main.js
@@ -10,40 +10,54 @@
 
 
 
+
+        // Image Slider 
+  $('.slider-active').slick({
+    dots: true,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+
+
+
+
+
+
+
+
+
 })(jQuery);
 
 
 
 
 
-
-
-//  var acc = document.getElementsByClassName("dropdown");
-//  var i;
-//  for (i = 0; i < acc.length; i++) {
-//    acc[i].addEventListener("click", function() {
-//      this.classList.toggle("active");
-//      var panel = this.nextElementSibling;
-//      if (panel.style.display === "block") {
-//        panel.style.display = "none";
-//      } else {
-//        panel.style.display = "block";
-//      };
-//    });
-//  }
-
-
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 
